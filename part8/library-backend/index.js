5f3d4905a32144e8f3f0a90a80a7a41823c9533c
Patrice Hermenault <hermenaultpatrice@gmail.com>
2021-11-10 17:31:53 +0100

    Finished exercise 8.13

---
+++ part8/library-backend/index.js
@@ -0,0 +1,215 @@
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
+
+const mongoose = require('mongoose')
+const Author = require('./models/author')
+const Book = require('./models/book')
+
+const MONGODB_URI = 'mongodb+srv://FlyingMandarine:Hellocs2_Mongo@cluster0.aouuh.mongodb.net/gql-library?retryWrites=true&w=majority'
+
+console.log('connecting to', MONGODB_URI)
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message)
+    })
+
+// let authors = [
+//     {
+//         name: 'Robert Martin',
+//         id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+//         born: 1952,
+//     },
+//     {
+//         name: 'Martin Fowler',
+//         id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+//         born: 1963
+//     },
+//     {
+//         name: 'Fyodor Dostoevsky',
+//         id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+//         born: 1821
+//     },
+//     { 
+//         name: 'Joshua Kerievsky', // birthyear not known
+//         id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+//     },
+//     { 
+//         name: 'Sandi Metz', // birthyear not known
+//         id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+//     },
+// ]
+
+// let books = [
+//     {
+//         title: 'Clean Code',
+//         published: 2008,
+//         author: 'Robert Martin',
+//         id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+//         genres: ['refactoring']
+//     },
+//     {
+//         title: 'Agile software development',
+//         published: 2002,
+//         author: 'Robert Martin',
+//         id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+//         genres: ['agile', 'patterns', 'design']
+//     },
+//     {
+//         title: 'Refactoring, edition 2',
+//         published: 2018,
+//         author: 'Martin Fowler',
+//         id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+//         genres: ['refactoring']
+//     },
+//     {
+//         title: 'Refactoring to patterns',
+//         published: 2008,
+//         author: 'Joshua Kerievsky',
+//         id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+//         genres: ['refactoring', 'patterns']
+//     },  
+//     {
+//         title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+//         published: 2012,
+//         author: 'Sandi Metz',
+//         id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+//         genres: ['refactoring', 'design']
+//     },
+//     {
+//         title: 'Crime and punishment',
+//         published: 1866,
+//         author: 'Fyodor Dostoevsky',
+//         id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+//         genres: ['classic', 'crime']
+//     },
+//     {
+//         title: 'The Demon ',
+//         published: 1872,
+//         author: 'Fyodor Dostoevsky',
+//         id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+//         genres: ['classic', 'revolution']
+//     },
+// ]
+
+const typeDefs = gql`
+    type Book {
+        title: String!
+        published: Int!
+        author: Author!
+        genres: [String!]!
+        id: ID!
+    }
+
+    type Author {
+        name: String!
+        id: ID!
+        born: Int
+        bookCount: Int!
+    }
+
+    type Query {
+        bookCount: Int!
+        authorCount: Int!
+        allBooks(author: String, genres: String): [Book]!
+        allAuthors: [Author!]!
+    }
+
+    type Mutation {
+        addBook(
+            title: String!
+            author: String!
+            published: Int!
+            genres: [String!]!
+        ): Book
+
+        editAuthor(
+            name: String!
+            setBornTo: Int!
+        ): Author
+    }
+`
+
+const resolvers = {
+    Query: {
+        bookCount: () => Book.collection.countDocuments(),
+        authorCount: () => Author.collection.countDocuments(),
+        allBooks: (root, args) => {
+            // let filteredBooks = books
+            
+            // if (args.author) {
+            //     filteredBooks = filteredBooks.filter((book) => book.author === args.author)
+            // }
+
+            // if (args.genres) {
+            //     filteredBooks = filteredBooks.filter((book => book.genres.includes(args.genres)))
+            // }
+
+            // return filteredBooks
+
+            return Book.find({})
+        },
+        allAuthors: () => {
+            return Author.find({})
+        }
+    },
+
+    Author: {
+        bookCount: (root) => {
+            const numberOfBooks = books.filter((book) => book.author === root.name)
+            return numberOfBooks.length
+        }
+    },
+
+    Mutation: {
+        addBook: async (root, args) => {
+            const author = await Author.findOne({ name: args.author })
+
+            const book = new Book({ ...args, author: author })
+
+            try {
+                await book.save()
+            } catch (error) {
+                throw new UserInputError(error.message, {
+                    invalidArgs: args,
+                })
+            }
+
+            return book
+
+            // const book = { ...args, id: uuid() }
+            // books = books.concat(book)
+
+            // if (!authors.some(a => a.name === args.author)) {
+            //     const author = { name: args.author, id: uuid() }
+            //     authors = authors.concat(author)
+            // }
+
+            // return book
+        },
+
+        editAuthor: (root, args) => {
+            const author = authors.find(a => a.name === args.name)
+            
+            if (!author) {
+                return null
+            }
+
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+            return updatedAuthor
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+})
