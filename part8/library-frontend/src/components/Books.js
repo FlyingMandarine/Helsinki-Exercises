fbc71425e6786f1b0e9376ff14eee0bd7b648f50
Patrice Hermenault <hermenaultpatrice@gmail.com>
2021-11-12 14:38:47 +0100

    Finished exercise 8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Books = ({ show, booksQuery }) => {
+  const [bookGenres, setBookGenres] = useState(null)
+  const [genre, setGenre] = useState('all genres')
+  const [filteredBooks, setFilteredBooks] = useState([])
+
+  useEffect(() => {
+    if (bookGenres) {
+      const booksToFilter = (genre) === 'all genres' ?
+        books :
+        books.filter(b => b.genres.includes(genre))
+
+      setFilteredBooks(booksToFilter)
+    }
+  }, [genre, bookGenres])
 
   if (!show) {
     return null
@@ -12,9 +25,33 @@ const Books = ({ show, booksQuery }) => {
 
   const books = booksQuery.data.allBooks
 
+  if (bookGenres === null) {
+    const allBookGenres = []
+    const bookGenresArray = books.map(b => b.genres)
+
+    bookGenresArray.forEach(genreArray => {
+      genreArray.forEach(genre => {
+        if (!allBookGenres.includes(genre)) {
+          allBookGenres.push(genre)
+        }
+      })
+    })
+
+    setBookGenres(allBookGenres)
+    return (null)
+  }
+
+  const filterGenre = (e) => {
+    setGenre(e.target.value)
+  }
+
   return (
     <div>
       <h2>books</h2>
+
+      {genre !== 'all genres' &&
+        <div>in genre <strong>{genre}</strong></div>
+      }
 
       <table>
         <tbody>
@@ -27,15 +64,32 @@ const Books = ({ show, booksQuery }) => {
               published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
+
+          {filteredBooks.map(a =>
+          <tr key={a.title}>
+            <td>{a.title}</td>
+            <td>{a.author.name}</td>
+            <td>{a.published}</td>
+          </tr>
           )}
+
         </tbody>
       </table>
+      <div>
+        {bookGenres.map(g =>
+          <button
+            key={g}
+            value={g}
+            onClick={(e) => filterGenre(e)}
+            >{g}
+          </button>
+        )}
+        <button
+          value={'all genres'}
+          onClick={(e) => filterGenre(e)}
+          >all genres
+        </button>
+      </div>
     </div>
   )
 }
